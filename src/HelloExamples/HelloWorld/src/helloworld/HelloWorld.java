1728d89733a7c3788cdfc1ea4538351f4552077e
Navdeep Gill <navdeep@h2o.ai>
2016-03-15 13:20:33 -0700

    Initial commit. HelloWorld & Print Statements

---
+++ src/HelloExamples/HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+package HelloExamples.HelloWorld.src.helloworld;
+
+/**
+ *
+ * @author navdeepgill
+ */
+public class HelloWorld {
+
+    /**
+     * main() function executes when Java program is run.
+     * 
+     * public = accessible to all
+     * static = does not need to be declared to be used
+     * void = returns nothing
+     */
+    public static void main(String[] args) {
+        //Example of one line comment.
+        System.out.println("Hello, World!!!!!");
+    }
+}
