692f0939fe7eb3ab370ddf39fe0e387afde73b0f
navdeep-G <mr.navdeepgill@gmail.com>
2016-03-16 12:01:44 -0700

    Common Data Types

---
+++ src/CommonDataTypes/src/BoolExample/Bool.java
@@ -0,0 +1,28 @@
+package CommonDataTypes.src.BoolExample;
+
+/**
+ * @author navdeepgill
+ */
+public class Bool {
+    public static void main(String args[]){
+        boolean b;
+
+        b = false;
+        System.out.println("b is " + b);
+        b = true;
+        System.out.println("b is " + b);
+
+        //Control if statement with boolean
+        if(b){
+            System.out.println("This is exectued");
+        }
+
+        b = false;
+        if(b){
+            System.out.println("This is not exectued");
+        }
+
+        //Outcome of a relational operator
+        System.out.println("10 > 9 is " + (10 > 9));
+    }
+}
