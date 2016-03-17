52d525e9ac7b9ae732723f5f74e26a748bf9d663
Navdeep Gill <navdeep@h2o.ai>
2016-03-17 09:53:04 -0700

    Relational Equality Conditional Operators

---
+++ src/RelationalEqualityConditionalOperators/src/relationalequalityconditionaloperators/RelationalQualityConditionalOperators.java
@@ -0,0 +1,25 @@
+package RelationalEqualityConditionalOperators.src.relationalequalityconditionaloperators;
+
+import java.util.Scanner;
+
+public class RelationalQualityConditionalOperators {
+
+    public static void main(String[] args) {
+        // demonstrates the relational, equality and conditional operators
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer value for X : ");
+        int x = input.nextInt();
+        System.out.print("Enter an integer value for Y : ");        
+        int y = input.nextInt();
+        
+        System.out.printf(" X == Y ? %s\n", x == y);
+        System.out.printf(" X != Y ? %s\n", x != y);
+        System.out.printf(" X > Y ? %s\n", x > y);
+        System.out.printf(" X <= Y ? %s\n", x <= y);
+        System.out.printf(" X >= Y ? %s\n", x >= y);
+        System.out.printf(" X < Y ? %s\n", x < y);
+        System.out.printf(" X == Y ? awesome : not awesome ? %s\n", x==y? "awesome" : "not awesome");
+        
+        
+    }
+}
