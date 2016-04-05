00b08f9ac38244ee7676de3f7dd6f8dba7e9ba8d
Navdeep Gill <navdeep@h2o.ai>
2016-04-05 11:37:02 -0700

    Unit Test Basics

---
+++ src/UnitTestBasics/src/UnitTestBasics/BlackBoxRun.java
@@ -0,0 +1,23 @@
+/*
+
+What is a unit test and why is it important? Unit tests are a means to verify 
+the methods we write are correct. We do this by writing code to verify our code 
+is correct. The methods we test are treated as a "Black Box" - 
+we know what is should do and this verify it performs as expected.
+
+ */
+package UnitTestBasics.src.UnitTestBasics;
+
+import UnitTestBasics.src.blackbox.BlackBox;
+import java.util.Scanner;
+
+public class BlackBoxRun {
+
+    public static void main(String[] args) {
+        BlackBox b = new BlackBox();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = input.nextInt();
+        System.out.printf("Number %d ==> %s\n",number, b.FizzBuzz(number));
+    }
+}
