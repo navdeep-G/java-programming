31582862619cdd9146dd1b0d0ebf187ceb688e4d
navdeep-G <mr.navdeepgill@gmail.com>
2016-07-08 09:17:33 -0700

    Build out a multiplication table

---
+++ src/MultTable/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package MultTable;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        int max = 10;
+        for (int i=1; i<=10; i++) {
+            for(int j=1;j<=10;j++) {
+                System.out.printf("%4d", i*j);
+            }
+            System.out.println();
+        }
+    }
+}
