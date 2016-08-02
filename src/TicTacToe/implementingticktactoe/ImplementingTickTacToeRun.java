6185214ff565f983947c49cb01923e40475ea822
Navdeep Gill <navdeep@h2o.ai>
2016-08-01 18:51:53 -0700

    Implement a tic tac toe game

---
+++ src/TicTacToe/implementingticktactoe/ImplementingTickTacToeRun.java
@@ -0,0 +1,12 @@
+package TicTacToe.implementingticktactoe;
+
+import TicTacToe.tictactoe.*;
+
+
+public class ImplementingTickTacToeRun {
+
+    public static void main(String[] args) {
+        Board b = new Board();
+        
+    }
+}
