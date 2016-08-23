0a191a435f6db5ea5d30934f1e6740c99b3d6416
Navdeep Gill <navdeep@h2o.ai>
2016-08-22 19:58:00 -0700

    Deck of playing cards

---
+++ src/DeckOfPlayingCards/playingcards/Suits.java
@@ -0,0 +1,13 @@
+package DeckOfPlayingCards.playingcards;
+
+/**
+ * Possible suits on the card
+ * @author nagill
+ */
+public class Suits {
+       
+    public static final int CLUBS =0;
+    public static final int DIAMONDS=1;
+    public static final int HEARTS=2;
+    public static final int SPADES=3;
+}
