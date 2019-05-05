4f5bb389b31ce3e43edaed2367534f83caac3893
Navdeep Gill <mr.navdeepgill@gmail.com>
2019-05-04 17:21:46 -0700

    Restructure repo (#1)
    * Restructure repo
    
    * Update README
    
    * Update README

---
+++ src/main/DrawTickTacToeBoard/DrawTickTacToeBoard.java
@@ -0,0 +1,25 @@
+ /* Write a java program which uses System.out to draw this tic-tac-toe board:
+ * 
+ *     |   |
+ *   X | X | O
+ *  ---+---+---
+ *   O | O |    
+ *  ---+---+---
+ *   X |   |   
+ *     |   |
+ */
+
+package DrawTickTacToeBoard;
+
+public class DrawTickTacToeBoard {
+
+    public static void main(String[] args) {
+        System.out.println("     |   |");
+        System.out.println("   X | X | 0");
+        System.out.println("  ---+---+---");
+        System.out.println("   0 | 0 |");   
+        System.out.println("  ---+---+---");
+        System.out.println("   X |   |");   
+        System.out.println("     |   |");
+    }
+}
