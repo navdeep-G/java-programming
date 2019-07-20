bc3f1c7527bfbb8ef3a54c9fc766870ac5e32bd2
navdeep-G <mr.navdeepgill@gmail.com>
2019-07-20 10:18:49 -0700

    Update objects

--- src/main/ProductCatalog/ProductCatalog.java
+++ src/main/ProductCatalog/ProductCatalog.java
@@ -6,9 +6,9 @@ public class ProductCatalog {
 
     public static void main(String[] args) {
         ArrayList<Product> products = new ArrayList<>();
-        products.add(new Book("John Smith",19.99, "12345",1250));
-        products.add(new Pants("Navdeep Gill", 59.99, 34,30));
-        products.add(new Product("Jackson Stuart", 1.99));
+        products.add(new Book("Jane Eyre",19.99, "12345",1250));
+        products.add(new Pants("Levis Jeans", 59.99, 34,30));
+        products.add(new Product("Lead Pencil", 1.99));
         
         System.out.printf("%-10s\t%10s\t%-10s\n","NAME","PRICE","TYPE");
         for (Product p : products) {
