4c2c66ebb49e9bfe32a7983d421f3779505fc5b8
navdeep-G <mr.navdeepgill@gmail.com>
2021-01-01 09:06:49 -0800

    Exception basics

---
+++ src/main/Exceptions/IntegerOutOfRangeException.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class IntegerOutOfRangeException extends Exception {
+
+    public IntegerOutOfRangeException(String errorMessage) {
+        super(errorMessage);
+    }
+}
